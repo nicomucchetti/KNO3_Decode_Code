3d35cbf2b0d57c8781c757456432a84c732f29b4
Nico Mucchetti <nico.mucchetti@gmail.com>
2026-01-31 11:06:42 -0500

    New Auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewAuto.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+
+
+
+import static android.os.SystemClock.sleep;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Autonomous(name = "NewAuto")
+public class NewAuto extends OpMode {
+    HALLayerSpeed hal = new HALLayerSpeed();
+
+
+    @Override
+    public void init() {
+
+        hal.initHardware(hardwareMap);
+        hal.startTLM(telemetry);
+    }
+
+    @Override
+    public void loop() {
+        double speed = gamepad1.right_stick_y * 0.95;
+        double strafe = gamepad1.right_stick_x * 0.95;
+        double turn = gamepad1.left_stick_x * 0.95;
+        hal.drive(speed, strafe, turn);
+
+        hal.addTLM("Flyspeed", hal.getSpeed(), telemetry);
+
+        hal.drive(-1.0f, 0.0f, 0.0f);
+        sleep(1300);
+        hal.turnOnIntake();
+        hal.turnOnShooter();
+        sleep(5000);
+        hal.turnOffIntake();
+        hal.turnOffShooter();
+        hal.drive(0.0f, -1.0f, 0.0f);
+       // telemetry.addData("BL Speed", hal.getBLSpeed());
+        hal.updateTLM(telemetry);
+        sleep(1);
+    }
+}
